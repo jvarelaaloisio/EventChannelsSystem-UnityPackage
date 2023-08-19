7c1471a190b8292e5222b47b4fca7e4b4f133eef
jvarelaaloisio <juampyvarela@gmail.com>
2023-08-19 18:40:29 -0300

    Adding createAssetMenu to idChannel

--- Runtime/Additions/Ids/IdChannelSo.cs
+++ Runtime/Additions/Ids/IdChannelSo.cs
@@ -1,5 +1,7 @@
 using Events.Runtime.Channels;
+using UnityEngine;
 
+[CreateAssetMenu(menuName = "Event Channels/Data Channels/Id", fileName = "IdChannel")]
 public class IdChannelSo : ChannelSo<Id>
 {
 }
