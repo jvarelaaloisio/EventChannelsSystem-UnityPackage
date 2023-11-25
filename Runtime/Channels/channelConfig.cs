01df7902bd055e3a24cb5cb3eb2390e4b64a6d8b
jvarelaaloisio <juampyvarela@gmail.com>
2023-11-25 19:01:51 -0300

    Fixing channel config

--- Runtime/Channels/channelConfig.cs
+++ Runtime/Channels/channelConfig.cs
@@ -6,7 +6,7 @@ namespace Events.Runtime.Channels
     [Serializable]
     public struct channelConfig
     {
-        [field: SerializeField] public bool LogSubscriptions { get; }
-        [field: SerializeField] public bool LogEventRisen { get; }
+        [field: SerializeField] public bool LogSubscriptions { get; set; }
+        [field: SerializeField] public bool LogEventRisen { get; set; }
     }
 }
